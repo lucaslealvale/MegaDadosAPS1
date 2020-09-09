6ccf37bd2168caaf5665844807e2c8557ed64e64
fontes99 <pvfontes1000@gmail.com>
2020-09-09 15:03:26 -0300

    uuid implement and metadata

--- main.py
+++ main.py
@@ -1,82 +1,90 @@
 from typing import Optional, List
 from fastapi import FastAPI
 from pydantic import BaseModel
+import uuid
 
-app = FastAPI()
+tags_metadata = [
+    {
+        "name": "Get tasks",
+        "description": "Listar tarefas"
+    },
+    {
+        "name": "Add tasks",
+        "description": "Adicionar tarefa"
+    },
+    {
+        "name": "Edit tasks",
+        "description": "Editar tarefa"
+    },
+    {
+        "name": "Remove tasks",
+        "description": "Remover tarefa"
+    }
+]
+
+app = FastAPI(
+    title="Megadados APS-1",
+    description="Feito por Pedro e Lucas",
+    openapi_tags=tags_metadata
+)
 
 class Tarefa(BaseModel):
     name: str
     descricao: str
 
+class TarefaDescricao(BaseModel):
+    descricao: str
+
 listona = {
-    'pegar_vovó_no_jiujistu' : {
-        'descricao' : 'vovó ta la no jiujitsu tem q ir buscar', 
-        'concluido' :  False 
+    uuid.uuid4().int : {
+        'nome'       : 'pegar vovó no jiujistu',
+        'descricao'  : 'vovó ta la no jiujitsu tem q ir buscar', 
+        'concluido'  :  False 
     },
 
-    'estudar_desComp' : {
-        'descricao' : 'ta mt osso', 
-        'concluido' :  False 
+    uuid.uuid4().int : {
+        'nome'       : 'estudar desComp',
+        'descricao'  : 'ta mt osso', 
+        'concluido'  :  False 
     },
 
-    'tomar_café' : {
-        'descricao' : 'vamo q vamo', 
-        'concluido' :  True 
+    uuid.uuid4().int : {
+        'nome'       : 'tomar café',
+        'descricao'  : 'vamo q vamo', 
+        'concluido'  :  True 
     }
 }
 
-@app.get("/") 
+@app.get("/", tags=["Get tasks"]) 
 async def read_root(checked: Optional[bool] = None):
-    
     if checked is not None:
-
         if checked:
-            ret = {k : v for k, v in listona.items() if v['concluido']}
-            return ret
+            return {k:v for k,v in listona.items() if v['concluido']}
         else:
-            ret = {k: v for k, v in listona.items() if not v['concluido']}
-            return ret
-
+            return {k:v for k,v in listona.items() if not v['concluido']}
     else:
         return listona
+    
+@app.get("/{uuid}", tags=["Get tasks"])
+async def read_task(uuid: int):
+    return {k:v for k,v in listona.items() if k == uuid}
 
-@app.post("/addItem/")
+@app.post("/addItem/", tags=["Add tasks"])
 async def create_item(tarefa: Tarefa):
-    listona[tarefa.name.replace(" ", "_")] = { 'descricao' : tarefa.descricao, 'concluido' : False }
+    listona[uuid.uuid4().int] = { 'nome' : tarefa.name, 'descricao' : tarefa.descricao, 'concluido' : False }
     return tarefa
 
-@app.patch("/checkItem/{tarefa_nome}")
-async def check_item(tarefa_nome: str):
-    listona[tarefa_nome]['concluido'] = not listona[tarefa_nome]['concluido']
-    return tarefa_nome + ' Checked'
+@app.patch("/checkItem/{uuid}", tags=["Edit tasks"])
+async def check_item(uuid: int):
+    listona[uuid]['concluido'] = not listona[uuid]['concluido']
+    return listona[uuid]['nome'] + ' Checked'
 
-@app.patch("/alterDescription/")
-async def alter_description(tarefa: Tarefa):
-    listona[tarefa.name.replace(" ", "_")]['descricao'] = tarefa.descricao
+@app.patch("/alterDescription/{uuid}", tags=["Edit tasks"])
+async def alter_description(uuid: int, tarefa: TarefaDescricao):
+    listona[uuid]['descricao'] = tarefa.descricao
     return 'description updated to: ' + tarefa.descricao
 
-@app.delete("/delTask/{tarefa_nome}")
-async def delete_task(tarefa_nome: str):
-    listona.pop(tarefa_nome)
-    return 'Task ~'+tarefa_nome+'~ Removed'
-
-# ---------  testes  -----------
-
-# @app.get("/files/{file_path:path}")
-# async def read_file(file_path: str):
-#     return {"file_path": file_path}
-
-# @app.get("/items/{item_id}")
-# async def read_querry(item_id: str, q: Optional[str] = None, short: bool = False):
-#     tarefa = {"item_id": item_id}
-#     if q:
-#         tarefa.update({"q": q})
-#     if not short:
-#         tarefa.update(
-#             {"description": "This is an amazing tarefa that has a long description"}
-#         )
-#     if short:
-#         tarefa.update(
-#             {"description": "blah"}
-#         )
-#     return tarefa
+@app.delete("/delTask/{uuid}", tags=["Remove tasks"])
+async def delete_task(uuid: int):
+    listona.pop(uuid)
+    return 'Task removed'
